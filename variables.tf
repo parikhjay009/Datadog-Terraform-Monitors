b486af2e96fadaeeeabe86118c10d68b54e899f8
parikhjay009 <47574935+parikhjay009@users.noreply.github.com>
2026-05-02 13:50:09 -0700

    Add RDS instance ID variable to variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,4 @@ variable "datadog_api_key" {}
 variable "datadog_app_key" {}
 variable "environment" { default = "production" }
 variable "cluster_name" { default = "your-eks-cluster" }
+variable "rds_instance_id" { default = "your-postgres-rds" }
